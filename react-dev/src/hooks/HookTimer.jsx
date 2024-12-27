f9e82637e0eedb04c75dc4287552c57f5e190dfa
Sukhdeb Patra <sukhdebpatra1@gmail.com>
2024-12-27 10:49:04 +0530

    changed in timer

--- react-dev/src/hooks/HookTimer.jsx
+++ react-dev/src/hooks/HookTimer.jsx
@@ -5,21 +5,47 @@ const HookTimer = () => {
 
   const intervalRef = useRef();
 
-  useEffect(() => {
+  //   useEffect(() => {
+
+  //   }, []);
+
+  const clareInterValHandler = () => {
+    clearInterval(intervalRef.current);
+    setTimer(0);
+  };
+  const startTimer = () => {
     intervalRef.current = setInterval(() => {
       setTimer((prevTimer) => prevTimer + 1);
     }, 1000);
     return () => {
       clearInterval(intervalRef.current);
     };
-  }, []);
+  };
   return (
     <>
       {" "}
       <h2>Timer example with useRef</h2>
       <div style={{ marginTop: "10px" }}>
         Hook Timer - {timer}
-        <button onClick={() => clearInterval(intervalRef.current)}>
+        <button
+          style={{
+            borderRadius: "10px",
+            backgroundColor: "violet",
+            marginLeft: "6%",
+          }}
+          onClick={startTimer}
+        >
+          Start TImer
+        </button>
+        <button
+          style={{
+            marginLeft: "1%",
+            borderRadius: "10px",
+            backgroundColor: "palevioletred",
+            marginTop: "3%",
+          }}
+          onClick={clareInterValHandler}
+        >
           Clear hook Timer
         </button>
       </div>
